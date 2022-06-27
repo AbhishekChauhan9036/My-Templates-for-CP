7682d70fa27cb7564dc691a924c0779fda27d15d
Abhi <abhishekchauhan9036@gmail.com>
2022-06-27 08:06:36 +0530

    commit message

--- advance-java.java
+++ advance-java.java
@@ -1,3 +1,12 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
 import java.util.*;
 import java.io.*;
 class Main{
@@ -15,3 +24,5 @@ public static void main (String[] args)throws IOException{
      }
    }
 }
+/******************************************************************************************************************************************/
+/***********End****************/
--- normal-java.java
+++ normal-java.java
@@ -1,3 +1,12 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
 import java.util.*;
 import java.io.*;
 class Main{
@@ -14,3 +23,5 @@ public static void main (String[] args)throws IOException{
      }
    }
 }
+/******************************************************************************************************************************************/
+/***********End****************/
