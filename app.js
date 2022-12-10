fc0bd24614355ca5c4b0b2f85a2d33e78c56e9ba
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-10 22:40:10 +0530

    profile link Updated

---
+++ app.js
@@ -0,0 +1,28 @@
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/* Author = Chauhan Abhishek */
+/* Codechef Id = https://www.codechef.com/users/abhishek_9036 */
+/* LeetCode Id = https://leetcode.com/abhishekchauhan9036/ */
+/* GeeksForGeeks Id = https://auth.geeksforgeeks.org/user/abhishekchauhan9036/profile */
+/* GitHub Id = https://github.com/AbhishekChauhan9036 */
+/* Language = Node.Js */
+/* Address = Ballia,UP,INDIA (221716)*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var arr = '';
+process.stdin.on('data', function (chunk) {
+    arr += chunk;
+});
+
+process.stdin.on('end', function () {
+    arr = arr.split('\n');
+    var t = Number(arr.shift());
+    while(t--){
+        let a=Number(arr.shift());
+        let b=Number(arr.shift());
+        let c=a-b
+        console.log(c);
+    }
+});
