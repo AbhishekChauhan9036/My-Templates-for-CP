1e6599dd17d9821c21d96c7f15a1e4cf4add0425
Abhishek Chauhan <96897831+AbhishekChauhan9036@users.noreply.github.com>
2022-12-31 17:42:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,19 +10,47 @@
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-
-var arr = '';
-process.stdin.on('data', function (chunk) {
-    arr += chunk;
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+process.stdin.on('data', function (data) {
+    input_stdin += data;
 });
 
 process.stdin.on('end', function () {
-    arr = arr.split('\n');
-    var t = Number(arr.shift());
-    while(t--){
-        let a=Number(arr.shift());
-        let b=Number(arr.shift());
-        let c=a-b
-        console.log(c);
-    }
+    input_stdin_array = input_stdin.split("\n");
+    main();
 });
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+    
+function main() {
+    var t = 1;
+    // t = parseInt(readLine());
+    while (t--) {
+        var n = parseInt(readLine());
+        var m = parseInt(readLine());
+        var arr1 = readLine().split(" ").map(Number);
+        var arr2 = readLine().split(" ").map(Number);
+        var res = solution(arr1, arr2, n,m);
+        console.log(res);
+    }
+}
+
+// Solution 1.......................................................
+function solution(arr1,arr2,n,m) {
+    let ans=[];
+    for(let i=0;i<n;i++){
+        if(ans.indexOf(arr1[i])==-1)
+        ans.push(arr1[i])
+    }
+
+    for(let i=0;i<m;i++){
+         if(ans.indexOf(arr2[i])==-1)
+            ans.push(arr2[i])
+    }
+    return ans.join(" ");
+}
